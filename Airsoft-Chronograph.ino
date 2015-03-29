2cb05465951a0e79874a6c3bb3b65cddbea65562
Albert Phan <albert.phan@hotmail.com>
2015-03-28 20:06:35 -0700

    Finished Averaging and MINMAX menu
    Also added drawScreen() for easier reading. To do: Rate of fire code and
    menu.

--- Airsoft-Chronograph.ino
+++ Airsoft-Chronograph.ino
@@ -27,7 +27,7 @@ AIN1 D7 Negative pin (reference pin) ~0.3-1V
 
 #define IRSPACING 0.080 //80 mm
 #define CLKPERIOD 0.0000000625  //62.5 nS 16MHz external Oscillator
-#define TIMEOUTTIME (50/4.096)	//50 ms/1 overflow time 4.096 mS
+#define TIMEOUTTIME (20/4.096)	//20 ms/1 overflow time 4.096 mS
 
 #define OK_BTN 5
 #define DOWN_BTN 4
@@ -36,11 +36,15 @@ AIN1 D7 Negative pin (reference pin) ~0.3-1V
 #define LED_PIN 13
 
 #define DEBOUNCE_TIME 10 // How many ms must pass before the button is considered stable
-#define REPEAT_INTERVAL 50 // How many ms in between button presses while holding it down
+//#define REPEAT_INTERVAL 50 // How many ms in between button presses while holding it down
 
-#define	NORMALIZED 0
-#define	RATEOFFIRE 1
+
+
+#define	NORMALIZED 3
 #define	AVERAGING 2
+#define MINMAX 1
+#define	RATEOFFIRE 0
+#define MAXMENUS 3
 
 Bounce okBtn(OK_BTN,DEBOUNCE_TIME);
 Bounce upBtn(UP_BTN, DEBOUNCE_TIME);
@@ -66,13 +70,17 @@ unsigned int bbCount = 0;
 
 double fps = 338.06;	//initially 338.06 for testing
 double normalizedfps = 0;
-double minfps = 0;
+double minfps = 10000;
 double maxfps = 0;
 //double averagefps = 0;
 double averageSum = 0;
 
 unsigned char bbWeight = 28;	//initially 0.28g as it is the most common
 unsigned char menuState = NORMALIZED;	//starts in normalized menu
+
+// Function Protoype
+void drawScreen(); // Handles drawing screen for various menus
+void fpsReady();	// Updates dataReady, bbcount, averagesum, min and max.
 
 
 ISR (ANALOG_COMP_vect) //When bb passes a beam.
@@ -87,11 +95,7 @@ ISR (ANALOG_COMP_vect) //When bb passes a beam.
 		// discard any ridiculous values
 		if(fps <= 5000)
 		{
-			dataReady = 1;
-			if(menuState == AVERAGING)
-			{
-				bbCount++;	//increments only in averaging mode
-			}
+			fpsReady();
 		}
 		else
 		{
@@ -136,7 +140,14 @@ void setup()
 	| bit(ACIS1)  // ACIS1, ACIS0: Analog Comparator Interrupt Mode Select (trigger on rising edge)
 	| bit(ACIS0);
 	
+	
+	// Pin Setups
 	pinMode(LED_PIN, OUTPUT); // Debugging led
+	pinMode(OK_BTN, INPUT);
+	pinMode(DOWN_BTN, INPUT);
+	pinMode(UP_BTN, INPUT);
+	pinMode(TEST_BTN, INPUT);
+	
 	
 	//16 bit Timer1 setup (used for bb fps calculations)
 	TCCR1A = 0;  //normal mode counts up to 0xFFFF and restarts
@@ -185,7 +196,7 @@ void loop()
 	downBtn.update();
 	testBtn.update();
 
-	///////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
 	// Button code for menu changes
 	
 	// if not editing a menu then up and down buttons change menu
@@ -200,13 +211,27 @@ void loop()
 			menuDecrementFlag = 1;
 		}
 	}
+	// If ok button is pressed in any menu except NORMALIZED then clear data
+	if(menuState != NORMALIZED && okBtn.rose())
+	{
+		fps = 0;
+		bbCount = 0;
+		averageSum = 0;
+		minfps = 10000;
+		maxfps = 0;
+		updateFlag = 1;
+	}
 	
 	if(menuIncrementFlag)
 	{
-		menuState++;
-		if(menuState > 2)
+		
+		if(menuState == MAXMENUS)
 		{
 			menuState = 0;
+		}
+		else 
+		{
+			menuState++;
 		}
 		menuIncrementFlag = 0;
 		updateFlag = 1;
@@ -214,48 +239,33 @@ void loop()
 	}
 	else if(menuDecrementFlag)
 	{
-		menuState--;
-		if(menuState > 2)
+		
+		if(menuState == 0)
 		{
-			menuState = 2;
+			menuState = MAXMENUS;
+		}
+		else
+		{
+			menuState--;
 		}
 		menuDecrementFlag = 0;
 		updateFlag = 1;
 		lcd.clear();
 	}
 	
-	// Testing button, used for generating fake fps numbers
-	//if(testBtn.rose())
-	//{
-		//fps = (double)((rand() % 500 + 1) + ((rand() % 101 + 1)/100.0));
-		//dataReady = 1;
-		//if(menuState == AVERAGING)
-		//{
-			//bbCount++;	//increments only in averaging mode
-		//}
-		//updateFlag = 1;
-	//}
-	//
-	// testing of retrigger seems to be broken?
-	if(testBtn.retrigger())
+	//Testing button, used for generating fake fps numbers
+	if(testBtn.rose())
 	{
 		fps = (double)((rand() % 500 + 1) + ((rand() % 101 + 1)/100.0));
-		dataReady = 1;
-		if(menuState == AVERAGING)
-		{
-			bbCount++;	//increments only in averaging mode
-		}
-		updateFlag = 1;
-		
-		// debugging 
-		ledState = 1;
-		digitalWrite(LED_PIN, ledState);
-		Serial.print(millis());
-		Serial.println(" Retriggered!");
+		fpsReady();
+	}
+	if(testBtn.retrigger())
+	{
+		fpsReady();
 	}
 
-	////////////////////////////////////////////////
-	// Menu specific code
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Normalized Menu specific code
 	if(menuState == NORMALIZED)
 	{
 		//button code for normalized menu
@@ -281,44 +291,17 @@ void loop()
 			{
 				bbDecrementFlag = 1;
 			}
+			
+			// if up or down button held down while editing, repeat button presses at REPEAT_INTERVAL
+			if(upBtn.retrigger())
+			{
+				bbIncrementFlag = 1;
+			}
+			else if(downBtn.retrigger())
+			{
+				bbDecrementFlag = 1;
+			}
 		}
-		// if up or down button held down while editing, repeat button presses at REPEAT_INTERVAL
-		
-		if(upBtn.retrigger() && editingFlag)
-		{
-			bbIncrementFlag = 1;
-		}
-		else if(downBtn.retrigger() && editingFlag)
-		{
-			bbDecrementFlag = 1;
-		}
-		
-		//if((upBtn.held() || downBtn.held()) && editingFlag)
-		//{
-		//
-		//if(previous_millis !=0)
-		//{
-		//if (millis() - previous_millis >= REPEAT_INTERVAL)
-		//{
-		//if(upBtn.held())
-		//{
-		//bbIncrementFlag = 1;	//increment button
-		//}
-		//else if(downBtn.held())
-		//{
-		//bbDecrementFlag = 1;	//decrement button
-		//}
-		//previous_millis = 0;
-		//}
-		//
-		//}
-		//else
-		//{
-		//previous_millis = millis();
-		//}
-		//
-		//
-		//}
 		
 		// Deals with changing bb weight
 		if(bbIncrementFlag)
@@ -343,52 +326,12 @@ void loop()
 			updateFlag = 1;
 		}
 		
-		/////////////////////////////////////////////////////////////
-		// Normalized FPS menu LCD stuff
 		if(updateFlag) //if anything changed
 		{
-			//Energy (in joules) = 1/2 mass * velocity^2
-			//therefore normalized Velocity (0.20g) = sqrt((0.5*bbweight*fps^2)*2/0.20)
-			normalizedfps = sqrt((0.5 * bbWeight * fps * fps)* 2 / 20);
-			
 			// Printing top row: "FPS|0.28g:XXX.XX"
+			//		bottom  row: "FPS|0.20g:XXX.XX"
 			//	column:			  01234567890123456
-			
-			lcd.home();
-			lcd.print("FPS|0.");
-			if(bbWeight < 10) //if single digit bb weight then add leading zero
-			{
-				lcd.print("0");
-			}
-			lcd.print(bbWeight);
-			lcd.print("g:");
-			lcd.print("      "); //clears the fps space
-			lcd.setCursor(10,0);
-			if(dataReady) //if no error
-			{
-				lcd.print(fps);
-			}
-			else
-			{
-				lcd.print("XXX.XX");
-			}
-			
-			
-			// Printing bottom row: "FPS|0.20g:XXX.XX"
-			lcd.setCursor(0,1);
-			lcd.print("FPS|0.20g:");
-			lcd.print("      "); //clears the fps space
-			lcd.setCursor(10,1);
-			if(dataReady) //if no error
-			{
-				lcd.print(normalizedfps);
-			}
-			else
-			{
-				lcd.print("XXX.XX");
-			}
-			// done updating
-			updateFlag = 0;
+			drawScreen();
 		}
 		
 		if(editingFlag)
@@ -402,58 +345,142 @@ void loop()
 		}
 		
 
-	}//end Normalized menu
-	//////////////////////////////////////////////////////////////////////////////////////////////
-	// Averaging Menu
+	}
 	else if(menuState == AVERAGING)
 	{
-		// Button stuff
-		if(okBtn.rose())
-		{
-			bbCount	= 0;
-			averageSum = 0;
-			fps = 0;
-			updateFlag = 1;
-		}
-		
 		if(updateFlag)
 		{
-			//if there is good data
-			if(dataReady)
-			{
-				averageSum += fps;
-			}
 			
-			// Printing top row "AVG|FPS:XXX.XX   "
-			//					 01234567890123456
-			lcd.home();
-			lcd.print("AVG|FPS:");
-			// if bbCount is cleared, just display 0.0 for avg fpss
-			lcd.print(bbCount == 0? 0.0 : averageSum / bbCount);
-			lcd.print("      "); // Clear rest of Row 1
-			
-			// Printing Bottom row "Max Diff:XXX.XX  "
-			//                      01234567890123456
-			lcd.setCursor(0,1);
-			lcd.print("BBs:");
-			lcd.print(bbCount);	//doing bbcounts for now instead of max diff
-			lcd.print(" FPS:");
-			lcd.print(fps); // For debugging
-			lcd.print("     ");
-			
-			updateFlag = 0;
+			// Printing top row "AVG:XXX.XX BB:   "
+			//		 bottom row "MAX Diff:XXX.XX"
+			//					 01234567890123456	
+			drawScreen();
 		}
 		
 		
 	}
+	else if (menuState == MINMAX)
+	{
+		// Printing top row "MIN:xxx.xx MAX:xxx.xx"
+		//					 01234567890123456
+		if (updateFlag)
+		{
+			drawScreen();
+		}
+	}
 	else if (menuState == RATEOFFIRE)
 	{
-		lcd.home();
-		lcd.print("Rate of fire menu");
+		if (updateFlag)
+		{
+			drawScreen();
+		}
 	}
 
 }
 
+void fpsReady()
+{
+	dataReady = 1;
+	bbCount++;
+	averageSum += fps;
+	
+	if(fps < minfps)
+	{
+		minfps = fps;
+	}
+	if(fps > maxfps)
+	{
+		maxfps = fps;
+	}
+	updateFlag = 1;
+}
+
+void drawScreen()
+{
+	if(menuState == NORMALIZED)
+	{
+		// Printing top row: "FPS|0.28g:XXX.XX"
+		lcd.home();
+		lcd.print("FPS|0.");
+		// If bbWeight is a single digit, add leading zero
+		if(bbWeight < 10)
+		{
+			lcd.print("0");
+		}
+		lcd.print(bbWeight);
+		lcd.print("g:");
+		lcd.print("      "); //clears the fps space
+		lcd.setCursor(10,0);
+		if(dataReady) //if no error
+		{
+			lcd.print(fps);
+		}
+		else
+		{
+			lcd.print("XXX.XX");
+		}
+		
+		// Printing bottom row: "FPS|0.20g:XXX.XX"
+		lcd.setCursor(0,1);
+		lcd.print("FPS|0.20g:");
+		lcd.print("      "); //clears the fps space
+		lcd.setCursor(10,1);
+		if(dataReady) //if fps data is good and ready
+		{
+			//Energy (in joules) = 1/2 mass * velocity^2
+			//therefore normalized Velocity (0.20g) = sqrt((0.5*bbweight*fps^2)*2/0.20)
+			lcd.print(sqrt((0.5 * bbWeight * fps * fps)* 2 / 20));
+		}
+		else
+		{
+			lcd.print("XXX.XX");
+		}
+		// done updating
+	}
+	else if(menuState == AVERAGING)
+	{
+		// Printing top row "AVG:XXX.XX BB:   "
+		//					 01234567890123456
+		lcd.home();
+		lcd.print("AVG:");
+		// if bbCount is cleared, just display 0.0 for avg fps
+		lcd.print(bbCount == 0? 0.0 : averageSum / bbCount);
+		lcd.print(" BB:");
+		lcd.print(bbCount);	//doing bbcounts for now instead of max diff
+		lcd.print("   "); // Clear rest of Row 1
+		
+		// Printing Bottom row "Max Diff:XXX.XX  "
+		//                      01234567890123456
+		lcd.setCursor(0,1);
+		lcd.print("MAX Diff:");
+		// If minfps represents real data then display it
+		lcd.print(minfps == 10000? 0.0: maxfps-minfps); // For debugging
+		lcd.print("     "); // Clear rest of row 2
+	}
+	else if(menuState == MINMAX)
+	{
+		// Printing top row "MIN:xxx.xx       "
+		//					 01234567890123456
+		// Printing bot row "MAX:xxx.xx       "
+		lcd.home();
+		lcd.print("MIN:");
+		// If minfps represents real data then display it
+		lcd.print(minfps == 10000? 0.0 :  minfps);
+		lcd.print("   "); //clear rest of row 1
+		lcd.setCursor(0,1);
+		lcd.print("MAX:");
+		lcd.print(maxfps);
+		lcd.print("   "); //clear rest of row 1
+		
+	}
+	else if (menuState == RATEOFFIRE)
+	{
+		
+		lcd.home();
+		lcd.print("RATEOFFIRE MENU");
+	}
+	updateFlag = 0;
+}
 
 
 
